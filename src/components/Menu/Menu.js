cca872542991cee78b2142947f1e8fb1bbd622a3
Naomi Campos <naomicampos@nkcampos.local>
2019-11-21 01:08:42 -0700

    Add styling to Menu component

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -3,13 +3,16 @@ import './Menu.css'
 
 const Menu = ({switchTopic}) => {
   return (
-    <div className='menu-items'>
-      <button id='local' onClick={switchTopic}>Local</button>
-      <button id='entertainment' onClick={switchTopic}>Entertainment</button>
-      <button id='science' onClick={switchTopic}>Science</button>
-      <button id='health' onClick={switchTopic}>Health</button>
-      <button id='technology' onClick={switchTopic}>Technology</button>
-    </div>
+    <nav>
+      <h1>What's New?</h1>
+      <div className='menu-btns'>
+        <button id='local' onClick={switchTopic}>Local</button>
+        <button id='entertainment' onClick={switchTopic}>Entertainment</button>
+        <button id='science' onClick={switchTopic}>Science</button>
+        <button id='health' onClick={switchTopic}>Health</button>
+        <button id='technology' onClick={switchTopic}>Technology</button>
+      </div>
+    </nav>
   )
 }
 
